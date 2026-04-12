d6606186607959f2b8a6d70245ebcced8a13f504
bturkan <bayramovat2005@gmail.com>
2026-04-12 13:30:47 +0400

    separate JS and CSS for header scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,10 +37,21 @@ const swiperHome = new Swiper('.home__swiper', {
           return '<span class="' + className + '">' + String(index + 1).padStart(2,'0') + "</span>";
         },
       },
+
+      // autoplay:{
+      //   delay:5000
+      // }
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+  const header = document.getElementById('header')
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50 ? header.classList.add('bg-header')
+                     : header.classList.remove('bg-header')
+}
 
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
